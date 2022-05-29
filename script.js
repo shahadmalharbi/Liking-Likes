986b56cab3ffd5285ad7038740a1409b8a90b641
Shahad Alharbi <103144756+shahadmalharbi@users.noreply.github.com>
2022-05-29 22:11:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+var counter1 = document.querySelector("#counter1")
+function Likes1() {
+    var likesCount = parseInt(counter1.innerText);
+    counter1.innerText = likesCount + 1;
+}
+var counter2 = document.querySelector("#counter2")
+function Likes2() {
+    var likesCount = parseInt(counter2.innerText);
+    counter2.innerText = likesCount + 1;
+}
+var counter3 = document.querySelector("#counter3")
+function Likes3() {
+    var likesCount = parseInt(counter3.innerText);
+    counter3.innerText = likesCount + 1;
+}
+
